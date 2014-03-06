24e1b5be2c1920ddd8ecc83d1d8361055e797a38
Gudny Lara Gudmundsdottir <gudnylara7@gmail.com>
2014-03-06 20:26:04 +0000

    Búin að bæta við smá mail hlutum. Appið krassar þegar þú ýtir á senda

--- TruinOgLifid/TruinOgLifid/src/is/tru/truin/FyrirbaenaefniFragment.java
+++ TruinOgLifid/TruinOgLifid/src/is/tru/truin/FyrirbaenaefniFragment.java
@@ -1,22 +1,27 @@
 package is.tru.truin;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class FyrirbaenaefniFragment extends Fragment implements OnClickListener {
 	
 	Button sendafyrirbaenButton;
+	EditText Fyrirbaen;
 	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
  
         View rootView = inflater.inflate(R.layout.fragment_fyrirbaenaefni, container, false);
+        
+        Fyrirbaen = (EditText) rootView.findViewById(R.id.fyrirbaenaefni);
         
         Button sendafyrirbaenButton = (Button) rootView.findViewById(R.id.sendafyrirbaenButton);
         sendafyrirbaenButton.setOnClickListener(this);     
@@ -28,6 +33,11 @@ public class FyrirbaenaefniFragment extends Fragment implements OnClickListener 
 	public void onClick(View v) {
 		//Hér verður emailið sent!
 		//sækja textann, vista senda í maili
+		
+		String baenin = Fyrirbaen.getText().toString();
+		
+		Intent postur = new Intent(Intent.ACTION_SEND);
+		postur.putExtra(Intent.EXTRA_TEXT, baenin);
 		
 	}
 
